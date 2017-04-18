4cff0eea71bdf4b642d94c0959109bfdb0512fee
Pablo Molina <p2kmgcl@gmail.com>
2017-04-18 20:39:00 +0200

    feat(docs): Updated index.d.ts documentation

--- src/index.d.ts
+++ src/index.d.ts
@@ -2,16 +2,47 @@ import {Map as ImmutableMap} from 'immutable'
 import {Selector} from 'reselect'
 import {Action, ActionCreator, Middleware} from 'redux'
 
-// Creating actions --------------------------------------------------------------------------------
-
+/**
+ * @type function
+ * Makes an action creator which allow dispatching actions of the given type.
+ * The generated function has a few special conditions:
+ *
+ * - The `toString()` method returns the action type that will be used
+ * - Any parameter passed to the function will be stored inside `action.meta.actionParameters[]`,
+ *   then, if the payload is an action (@see functionMiddleware), it will receive the list of
+ *   parameters. This way you can process them either inside the payload or a custom created
+ *   reducer.
+ *
+ * @param {string} type Action type of that will be used for the dispatched actions
+ * @param {object} [meta={}] Information of every dispatched action. This attribute is part
+ *  of a "Flux Standard Action", and is used by redux scalable for some extra information providing
+ *  (like the `actionParameters` array). You can add any extra information if you want.
+ * @param {object|Promise|function} [payload={}] The content itself. As payloads represent the main
+ *  part of the actions, the vast majority of changes happen here. If you specify a function, it
+ *  will be executed with `actionParameters` (@see functionMiddleware), if you use a promise, it
+ *  will be resolved/rejected (@see promiseMiddleware), and if you use a simple object... well,
+ *  nothing will happen :). Generally you want to wrap a promise inside a function to prevent it
+ *  from immediately start, or you may use the fancy `async` operator.
+ *
+ * @return {ActionCreator} The generated action creator with the described functionality.
+ */
 function makeActionCreator(
   type: string,
   meta: object = {},
   payload: object|Promise|Function = {}
 ): ActionCreator
 
-// Function middleware -----------------------------------------------------------------------------
-
+/**
+ * @type function
+ * Redux Middleware that allows processing actions' payloads as function by executing them
+ * before being sent to the store. When a function payload is received by this middleware, it
+ * checks if the corresponding action has an `meta.actionParameters` property. If it happens,
+ * this array of parameters is passed to the function payload. Then, the result of this execution
+ * is set as real payload and the action is passed to the next middleware.
+ *
+ * For using this middleware just inject it to redux using the `applyMiddleware` function provided
+ * by them.
+ */
 interface functionMiddleware extends Middleware {
 }
 
@@ -112,7 +143,7 @@ interface ActionFragmentCreator {
   setKeyPath: (keyPath: string[]|null) => void
 }
 
-interface defineActionFragmentCreator {
+interface ActionFragmentCreatorDefinition {
   type: string,
   initialState: any,
   makeFragment: (...args:any[]) => ActionFragment,
@@ -120,7 +151,7 @@ interface defineActionFragmentCreator {
   selector: Selector<any, any>,
 }
 
-function defineActionFragment(defineActionFragmentCreator): ActionFragmentCreator
+function defineActionFragment(ActionFragmentCreatorDefinition): ActionFragmentCreator
 
 // Export ------------------------------------------------------------------------------------------
 
